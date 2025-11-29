f952a6125b2a782d061f20ec924edd875b733c0b
kristij-98 <kristi@bandakreative.com>
2025-11-29 22:49:30 +0000

    Allow hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: true
+  },
+  server: {
+    host: true,
+    allowedHosts: true
+  }
+})
